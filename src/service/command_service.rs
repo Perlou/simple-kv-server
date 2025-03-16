c6f43aeeefa06d52d83b15631a643c6678aab079
Perlou <perloukevin@gmail.com>
2025-03-17 01:12:31 +0800

    feat service

---
+++ src/service/command_service.rs
@@ -0,0 +1,126 @@
+use crate::*;
+
+impl CommandService for Hget {
+    fn execute(self, store: &impl Storage) -> CommandResponse {
+        match store.get(&self.table, &self.key) {
+            Ok(Some(v)) => v.into(),
+            Ok(None) => KvError::NotFound(format!("table {}, key {}", self.table, self.key)).into(),
+            Err(e) => e.into(),
+        }
+    }
+}
+
+impl CommandService for Hmget {
+    fn execute(self, store: &impl Storage) -> CommandResponse {
+        self.keys
+            .iter()
+            .map(|key| match store.get(&self.table, key) {
+                Ok(Some(v)) => v,
+                _ => Value::default(),
+            })
+            .collect::<Vec<_>>()
+            .into()
+    }
+}
+
+impl CommandService for Hgetall {
+    fn execute(self, store: &impl Storage) -> CommandResponse {
+        match store.get_all(&self.table) {
+            Ok(v) => v.into(),
+            Err(e) => e.into(),
+        }
+    }
+}
+
+impl CommandService for Hset {
+    fn execute(self, store: &impl Storage) -> CommandResponse {
+        match self.pair {
+            Some(v) => match store.set(&self.table, v.key, v.value.unwrap_or_default()) {
+                Ok(Some(v)) => v.into(),
+                Ok(None) => Value::default().into(),
+                Err(e) => e.into(),
+            },
+            None => KvError::InvalidCommand(format!("{:?}", self)).into(),
+        }
+    }
+}
+
+impl CommandService for Hmset {
+    fn execute(self, store: &impl Storage) -> CommandResponse {
+        let pairs = self.pairs;
+        let table = self.table;
+        pairs
+            .into_iter()
+            .map(|pair| {
+                let result = store.set(&table, pair.key, pair.value.unwrap_or_default());
+                match result {
+                    Ok(Some(v)) => v,
+                    _ => Value::default(),
+                }
+            })
+            .collect::<Vec<_>>()
+            .into()
+    }
+}
+
+impl CommandService for Hdel {
+    fn execute(self, store: &impl Storage) -> CommandResponse {
+        match store.del(&self.table, &self.key) {
+            Ok(Some(v)) => v.into(),
+            Ok(None) => Value::default().into(),
+            Err(e) => e.into(),
+        }
+    }
+}
+
+impl CommandService for Hmdel {
+    fn execute(self, store: &impl Storage) -> CommandResponse {
+        self.keys
+            .iter()
+            .map(|key| match store.del(&self.table, key) {
+                Ok(Some(v)) => v,
+                _ => Value::default(),
+            })
+            .collect::<Vec<_>>()
+            .into()
+    }
+}
+
+impl CommandService for Hexist {
+    fn execute(self, store: &impl Storage) -> CommandResponse {
+        match store.contains(&self.table, &self.key) {
+            Ok(v) => Value::from(v).into(),
+            Err(e) => e.into(),
+        }
+    }
+}
+
+impl CommandService for Hmexist {
+    fn execute(self, store: &impl Storage) -> CommandResponse {
+        self.keys
+            .iter()
+            .map(|key| match store.contains(&self.table, key) {
+                Ok(v) => v.into(),
+                _ => Value::default(),
+            })
+            .collect::<Vec<Value>>()
+            .into()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn hget_should_work() {}
+
+    fn set_key_pairs<T: Into<Value>>(table: &str, pairs: Vec<(&str, T)>, store: &impl Storage) {
+        pairs
+            .into_iter()
+            .map(|(k, v)| CommandRequest::new_hset(table, k, v.into()))
+            .for_each(|cmd| {
+                // dispatch(cmd, store);
+            });
+    }
+}
---
+++ src/service/mod.rs
@@ -0,0 +1,102 @@
+use crate::{
+    command_request::RequestData, CommandRequest, CommandResponse, KvError, MemTable, Storage,
+};
+use futures::stream;
+use std::sync::Arc;
+use tracing::{debug, instrument};
+
+mod command_service;
+mod topic;
+mod topic_service;
+
+pub use topic::{Broadcaster, Topic};
+pub use topic_service::{StreamingResponse, TopicService};
+
+/// 对 Command 的处理的抽象
+pub trait CommandService {
+    /// 处理 Command，返回 Response
+    fn execute(self, store: &impl Storage) -> CommandResponse;
+}
+
+/// 事件通知（不可变事件）
+pub trait Notify<Arg> {
+    fn notify(&self, arg: &Arg);
+}
+
+/// 事件通知（可变事件）
+pub trait NotifyMut<Arg> {
+    fn notify(&self, arg: &mut Arg);
+}
+
+impl<Arg> Notify<Arg> for Vec<fn(&Arg)> {
+    #[inline]
+    fn notify(&self, arg: &Arg) {
+        for f in self {
+            f(arg)
+        }
+    }
+}
+
+impl<Arg> NotifyMut<Arg> for Vec<fn(&mut Arg)> {
+    #[inline]
+    fn notify(&self, arg: &mut Arg) {
+        for f in self {
+            f(arg)
+        }
+    }
+}
+
+/// Service 内部数据结构
+pub struct ServiceInner<Store> {
+    store: Store,
+    on_received: Vec<fn(&CommandRequest)>,
+    on_executed: Vec<fn(&CommandResponse)>,
+    on_before_send: Vec<fn(&mut CommandResponse)>,
+    on_after_send: Vec<fn()>,
+}
+
+impl<Store: Storage> ServiceInner<Store> {
+    pub fn new(store: Store) -> Self {
+        Self {
+            store,
+            on_received: Vec::new(),
+            on_executed: Vec::new(),
+            on_before_send: Vec::new(),
+            on_after_send: Vec::new(),
+        }
+    }
+
+    pub fn fn_received(mut self, f: fn(&CommandRequest)) -> Self {
+        self.on_received.push(f);
+        self
+    }
+
+    pub fn fn_executed(mut self, f: fn(&CommandResponse)) -> Self {
+        self.on_executed.push(f);
+        self
+    }
+
+    pub fn fn_before_send(mut self, f: fn(&mut CommandResponse)) -> Self {
+        self.on_before_send.push(f);
+        self
+    }
+
+    pub fn fn_after_send(mut self, f: fn()) -> Self {
+        self.on_after_send.push(f);
+        self
+    }
+}
+
+// impl<Store: Storage> From<ServiceInner<Store>> for Service<Store> {
+//     fn from(inner: ServiceInner<Store>) -> Self {
+//         Self {
+//             inner: Arc::new(inner),
+//             broadcaster: Default::default(),
+//         }
+//     }
+// }
+
+// impl<Store: Storage> Service<Store> {
+//     #[instrument(name = "service_execute", skip_all)]
+//     pub fn execute(&self, cmd: CommandRequest) -> Se
+// }
