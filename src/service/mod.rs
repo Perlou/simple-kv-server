cfc76e67b979ccc8f00f5e52f6e1e82c06208630
Perlou <perloukevin@gmail.com>
2025-03-17 10:07:43 +0800

    feat service mod

--- src/service/mod.rs
+++ src/service/mod.rs
@@ -46,6 +46,21 @@ impl<Arg> NotifyMut<Arg> for Vec<fn(&mut Arg)> {
     }
 }
 
+/// Service 数据结构
+pub struct Service<Store = MemTable> {
+    inner: Arc<ServiceInner<Store>>,
+    broadcaster: Arc<Broadcaster>,
+}
+
+impl<Store> Clone for Service<Store> {
+    fn clone(&self) -> Self {
+        Self {
+            inner: Arc::clone(&self.inner),
+            broadcaster: Arc::clone(&self.broadcaster),
+        }
+    }
+}
+
 /// Service 内部数据结构
 pub struct ServiceInner<Store> {
     store: Store,
@@ -87,16 +102,62 @@ impl<Store: Storage> ServiceInner<Store> {
     }
 }
 
-// impl<Store: Storage> From<ServiceInner<Store>> for Service<Store> {
-//     fn from(inner: ServiceInner<Store>) -> Self {
-//         Self {
-//             inner: Arc::new(inner),
-//             broadcaster: Default::default(),
-//         }
-//     }
-// }
+impl<Store: Storage> From<ServiceInner<Store>> for Service<Store> {
+    fn from(inner: ServiceInner<Store>) -> Self {
+        Self {
+            inner: Arc::new(inner),
+            broadcaster: Default::default(),
+        }
+    }
+}
 
-// impl<Store: Storage> Service<Store> {
-//     #[instrument(name = "service_execute", skip_all)]
-//     pub fn execute(&self, cmd: CommandRequest) -> Se
-// }
+impl<Store: Storage> Service<Store> {
+    #[instrument(name = "service_execute", skip_all)]
+    pub fn execute(&self, cmd: CommandRequest) -> StreamingResponse {
+        debug!("Got request: {:?}", cmd);
+        self.inner.on_received.notify(&cmd);
+        let mut res = dispatch(cmd.clone(), &self.inner.store);
+
+        if res == CommandResponse::default() {
+            dispatch_stream(cmd, Arc::clone(&self.broadcaster))
+        } else {
+            debug!("Executed response: {:?}", res);
+            self.inner.on_executed.notify(&res);
+            self.inner.on_before_send.notify(&mut res);
+            if !self.inner.on_before_send.is_empty() {
+                debug!("Modified response: {:?}", res);
+            }
+
+            Box::pin(stream::once(async { Arc::new(res) }))
+        }
+    }
+}
+
+/// 从 Request 中得到 Response，目前处理所有 HGET/HSET/HDEL/HEXIST
+pub fn dispatch(cmd: CommandRequest, store: &impl Storage) -> CommandResponse {
+    match cmd.request_data {
+        Some(RequestData::Hget(param)) => param.execute(store),
+        Some(RequestData::Hgetall(param)) => param.execute(store),
+        Some(RequestData::Hmget(param)) => param.execute(store),
+        Some(RequestData::Hset(param)) => param.execute(store),
+        Some(RequestData::Hmset(param)) => param.execute(store),
+        Some(RequestData::Hdel(param)) => param.execute(store),
+        Some(RequestData::Hmdel(param)) => param.execute(store),
+        Some(RequestData::Hexist(param)) => param.execute(store),
+        Some(RequestData::Hmexist(param)) => param.execute(store),
+        None => KvError::InvalidCommand("Request has no data".into()).into(),
+        // 处理不了的返回一个啥都不包括的 Response，这样后续可以用 dispatch_stream 处理
+        _ => CommandResponse::default(),
+    }
+}
+
+/// 从 Request 中得到 Response，目前处理所有 PUBLISH/SUBSCRIBE/UNSUBSCRIBE
+pub fn dispatch_stream(cmd: CommandRequest, topic: impl Topic) -> StreamingResponse {
+    match cmd.request_data {
+        Some(RequestData::Publish(param)) => param.execute(topic),
+        Some(RequestData::Subscribe(param)) => param.execute(topic),
+        Some(RequestData::Unsubscribe(param)) => param.execute(topic),
+        // 如果走到这里，就是代码逻辑的问题，直接 crash 出来
+        _ => unreachable!(),
+    }
+}
