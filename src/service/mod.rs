cce1fe0a393c687625c27ed836d0d1b4e6f3b42c
Perlou <perloukevin@gmail.com>
2025-03-17 16:57:16 +0800

    feat service done

--- src/service/mod.rs
+++ src/service/mod.rs
@@ -161,3 +161,89 @@ pub fn dispatch_stream(cmd: CommandRequest, topic: impl Topic) -> StreamingRespo
         _ => unreachable!(),
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use http::StatusCode;
+    use tokio_stream::StreamExt;
+    use tracing::info;
+
+    use super::*;
+    use crate::{MemTable, Value};
+
+    #[tokio::test]
+    async fn service_should_works() {
+        // 我们需要一个 service 结构至少包含 Storage
+        let service: Service = ServiceInner::new(MemTable::default()).into();
+
+        // service 可以运行在多线程环境下，它的 clone 应该是轻量级的
+        let cloned = service.clone();
+
+        // 创建一个线程，在 table t1 中写入 k1, v1
+        tokio::spawn(async move {
+            let mut res = cloned.execute(CommandRequest::new_hset("t1", "k1", "v1".into()));
+            let data = res.next().await.unwrap();
+            assert_res_ok(&data, &[Value::default()], &[]);
+        })
+        .await
+        .unwrap();
+
+        // 在当前线程下读取 table t1 的 k1，应该返回 v1
+        let mut res = service.execute(CommandRequest::new_hget("t1", "k1"));
+        let data = res.next().await.unwrap();
+        assert_res_ok(&data, &["v1".into()], &[]);
+    }
+
+    #[tokio::test]
+    async fn event_registration_should_work() {
+        fn b(cmd: &CommandRequest) {
+            info!("Got {:?}", cmd);
+        }
+        fn c(res: &CommandResponse) {
+            info!("{:?}", res);
+        }
+        fn d(res: &mut CommandResponse) {
+            res.status = StatusCode::CREATED.as_u16() as _;
+        }
+        fn e() {
+            info!("Data is sent");
+        }
+
+        let service: Service = ServiceInner::new(MemTable::default())
+            .fn_received(|_: &CommandRequest| {})
+            .fn_received(b)
+            .fn_executed(c)
+            .fn_before_send(d)
+            .fn_after_send(e)
+            .into();
+
+        let mut res = service.execute(CommandRequest::new_hset("t1", "k1", "v1".into()));
+        let data = res.next().await.unwrap();
+        assert_eq!(data.status, StatusCode::CREATED.as_u16() as u32);
+        assert_eq!(data.message, "");
+        assert_eq!(data.values, vec![Value::default()]);
+    }
+}
+
+#[cfg(test)]
+use crate::{Kvpair, Value};
+
+// 测试成功返回的结果
+#[cfg(test)]
+pub fn assert_res_ok(res: &CommandResponse, values: &[Value], pairs: &[Kvpair]) {
+    let mut sorted_pairs = res.pairs.clone();
+    sorted_pairs.sort_by(|a, b| a.partial_cmp(b).unwrap());
+    assert_eq!(res.status, 200);
+    assert_eq!(res.message, "");
+    assert_eq!(res.values, values);
+    assert_eq!(sorted_pairs, pairs);
+}
+
+// 测试失败返回的结果
+#[cfg(test)]
+pub fn assert_res_error(res: &CommandResponse, code: u32, msg: &str) {
+    assert_eq!(res.status, code);
+    assert!(res.message.contains(msg));
+    assert_eq!(res.values, &[]);
+    assert_eq!(res.pairs, &[]);
+}
